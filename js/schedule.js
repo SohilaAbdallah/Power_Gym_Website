207e1770c56b3f2a0002796e600f686f4c9d9670
SohilaAbdallah <110264357+SohilaAbdallah@users.noreply.github.com>
2023-05-08 02:27:38 +0300

    Add files via upload

---
+++ js/schedule.js
@@ -0,0 +1,131 @@
+      //box1
+      //when click in anu box img display
+      document.querySelectorAll('.click  .square1').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule1').style.display = 'block';
+            // document.querySelector('.scadule img').src=  "background.png" ;
+        }
+    });
+
+
+
+    //close the img
+    document.querySelector('.scadule1 span').onclick = () => {
+        document.querySelector('.scadule1').style.display = 'none';
+    }
+
+
+
+    //box2
+    document.querySelectorAll('.click  .square2').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule2').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule2 span').onclick = () => {
+        document.querySelector('.scadule2').style.display = 'none';
+    }
+
+
+
+
+    //box3
+    document.querySelectorAll('.click  .square3').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule3').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule3 span').onclick = () => {
+        document.querySelector('.scadule3').style.display = 'none';
+    }
+
+    //box4
+    document.querySelectorAll('.click  .square4').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule4').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule4 span').onclick = () => {
+        document.querySelector('.scadule4').style.display = 'none';
+    }
+
+    //box5
+    document.querySelectorAll('.click  .square5').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule5').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule5 span').onclick = () => {
+        document.querySelector('.scadule5').style.display = 'none';
+    }
+
+    //box6
+    document.querySelectorAll('.click2  .square6').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule6').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule6 span').onclick = () => {
+        document.querySelector('.scadule6').style.display = 'none';
+    }
+    //box7
+    document.querySelectorAll('.click2  .square7').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule7').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule7 span').onclick = () => {
+        document.querySelector('.scadule7').style.display = 'none';
+    }
+    //box8
+    document.querySelectorAll('.click2  .square8').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule8').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule8 span').onclick = () => {
+        document.querySelector('.scadule8').style.display = 'none';
+    }
+    //box9
+    document.querySelectorAll('.click2  .square9').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule9').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule9 span').onclick = () => {
+        document.querySelector('.scadule9').style.display = 'none';
+    }
+    //box10
+    document.querySelectorAll('.click2  .square10').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.scadule10').style.display = 'block';
+
+        }
+    });
+    //close the img
+    document.querySelector('.scadule10 span').onclick = () => {
+        document.querySelector('.scadule10').style.display = 'none';
+    }
+
+
+
+
+
+
+
